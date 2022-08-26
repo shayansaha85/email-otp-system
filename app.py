30e44b47562e1240e81d74104c90728c0837ab28
Shayan Saha <shayan851997@gmail.com>
2022-08-26 19:53:18 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,88 @@
+from flask import *
+import smtplib, ssl
+import random
+from jproperties import Properties
+
+
+app = Flask(__name__)
+
+otp = {"otp" : ""}
+
+def getCreds():
+    configs = Properties()
+
+    with open('config.properties', 'rb') as config_file:
+        configs.load(config_file)
+
+    sender_email = str(configs.get("sender_email").data)
+    sender_password = str(configs.get("sender_password").data)
+    email_server_url = str(configs.get("email_server_url").data)
+
+    creds = {
+        "sender_email" : "",
+        "sender_password" : "",
+        "email_server_url" : ""
+    }
+
+    creds["sender_email"] = sender_email
+    creds["sender_password"] = sender_password
+    creds["email_server_url"] = email_server_url
+
+    return creds
+
+
+def generate_otp():
+    otp = ""
+    i = 0
+    while i<6:
+        otp = otp + str(random.randint(0,9))
+        i=i+1
+    return otp
+
+
+def send_otp(sender_email, sender_password, to_email, smtp_server, otp):
+    print("**********DEBUG MESSAGE : send_otp() METHOD STARTED**********")
+    port = 465
+    message = """
+    Subject: OTP
+
+    Your OTP for verification : {otp}""".format(otp=otp)
+
+    context = ssl.create_default_context()
+    try:
+        with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+            server.login(sender_email, sender_password)
+            server.sendmail(sender_email, to_email, message)
+        print("**********DEBUG MESSAGE : EMAIL SENT SUCCESSFULLY**********")
+    except:
+        print("********DEBUG MESSAGE : EMAIL SENDING FAILED********")
+
+
+@app.route("/home", methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
+        to_email = str(request.form.get("email"))
+        otp["otp"] = generate_otp()
+        creds = getCreds()
+        sender_email = creds["sender_email"]
+        sender_password = creds["sender_password"]
+        email_server_url = creds["email_server_url"]
+        send_otp(sender_email, sender_password, to_email, email_server_url, otp["otp"])
+        return redirect(url_for("otpVerification"))
+    return render_template("index.html")
+
+
+
+@app.route("/otpVerification", methods=["GET", "POST"])
+def otpVerification():
+    outcome = ""
+    if request.method == "POST":
+        userOTP = str(request.form.get("otp"))
+        if userOTP == otp["otp"]:
+            outcome = "true"
+        print(outcome)
+    return render_template("otpVerificationPage.html", outcome=outcome)
+
+
+
+app.run(debug=True, host="0.0.0.0")
